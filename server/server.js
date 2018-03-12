1717225237310c8bbda130f478dfcd78526ec941
Petru <escu.petru@gmail.com>
2018-03-12 20:16:53 +0200

    se tup

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public')
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
@@ -10,7 +11,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000, ()=> {
+app.listen(port, ()=> {
     console.log('server up');
     
-})
+});
