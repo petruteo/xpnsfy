0fc1990c019d226746e3a3c7cc90650e2cea753b
Petru <escu.petru@gmail.com>
2018-03-13 14:58:33 +0200

    totals

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
-import ExpenseListFilters from './ExpenseListFilters'; 
+import ExpenseListFilters from './ExpenseListFilters';
+import { connect } from 'react-redux';
+import selectExpenses from '../selectors/expenses';
+import numeral from 'numeral';
 
-const ExpenseDashboardPage = () => (
-    <div>
-    This is from my dahsboard component.
-    <ExpenseListFilters />
-    <ExpenseList />
-    </div>
+const mapStateToProps = (state) => {
+	// console.log('******', state.expenses, state.expenses.length);
+
+	return {
+		expenses: selectExpenses(state.expenses, state.filters)
+	};
+};
+
+const ExpenseDashboardPage = (props) => (
+	<div>
+		<h1>
+			View {props.expenses.length} expenses totaling{' '}
+			{numeral(props.expenses.map((expense) => expense.amount).reduce((a, b) => a + b, 0)).format('$0,0.00')}.
+		</h1>
+		{console.log(props.expenses.length)}
+
+		<ExpenseListFilters />
+		<ExpenseList />
+	</div>
 );
 
-export default ExpenseDashboardPage;
+export default connect(mapStateToProps)(ExpenseDashboardPage);
